e3faa69b7b00d6d50d15cebaa85c9d37df91b140
Teemo Tebest <teemo.tebest@gmail.com>
2022-08-03 13:48:17 +0200

    Added meta data

--- webpack.common.js
+++ webpack.common.js
@@ -72,12 +72,9 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'assets/img/', to: '../public/assets/img/', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store'],
-          }},
-        { from: 'src/font/', to: '../public/font/', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store'],
-          }},
-        { from: './favicon.png', to: '../public', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store'],
-          }}
+        { from: 'assets/img/', to: '../public/assets/img/', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store']}},
+        { from: 'src/font/', to: '../public/font/', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store']}},
+        { from: './favicon.png', to: '../public', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store']}}
       ]
     })
   ]
