7c21cbaccbfd2976468268b7596b0c0b35b3d950
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-05 23:05:18 +0200

    Cleaned code

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,4 +1,4 @@
-import React, { /* useState, */useEffect } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import '../styles/styles.less';
 
 // https://d3js.org/
@@ -7,26 +7,10 @@ import * as d3 from 'd3';
 import * as Datamap from 'datamaps';
 import { geoRobinson } from 'd3-geo-projection';
 
-let map;
 function App() {
-  const initMap = () => {
-    map = new Datamap({
-      element: document.querySelector('.map'),
-      responsive: true,
-      geographyConfig: {
-        highlightOnHover: false,
-        popupOnHover: true
-      },
-      setProjection(element) {
-        const projection = geoRobinson().center([0, 0]).scale(element.offsetWidth / 6).translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-        const path = d3.geoPath().projection(projection);
-        return { path, projection };
-      },
-      // projection: 'georobinson',
-      fills: {
-        authorHasTraveledTo: 'rgba(114, 159, 152, 1.0)',
-        defaultFill: 'rgba(188, 213, 209, 1.0)'
-      },
+  const map = useRef();
+  const initMap = useCallback(() => {
+    map.current = new Datamap({
       data: { // http://www.worldatlas.com/aatlas/ctycodes.htm
         AUT: { fillKey: 'authorHasTraveledTo' },
         BEL: { fillKey: 'authorHasTraveledTo' },
@@ -52,15 +36,31 @@ function App() {
         TUN: { fillKey: 'authorHasTraveledTo' },
         TUR: { fillKey: 'authorHasTraveledTo' },
         USA: { fillKey: 'authorHasTraveledTo' }
+      },
+      element: document.querySelector('.map'),
+      fills: {
+        authorHasTraveledTo: 'rgba(114, 159, 152, 1.0)',
+        defaultFill: 'rgba(188, 213, 209, 1.0)'
+      },
+      geographyConfig: {
+        highlightOnHover: false,
+        popupOnHover: true
+      },
+      responsive: true,
+      setProjection(element) {
+        const projection = geoRobinson().center([0, 0]).scale(element.offsetWidth / 6).translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+        const path = d3.geoPath().projection(projection);
+        return { path, projection };
       }
+      // projection: 'georobinson',
     });
-  };
+  }, []);
   useEffect(() => {
     initMap();
     window.addEventListener('resize', (event) => {
-      map.resize(event);
+      map.current.resize(event);
     });
-  }, []);
+  }, [initMap, map]);
 
   return (
     <div className="app">
